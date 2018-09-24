90416cbd453c66062fc4126f63dc666ca1ebc068
grijdaspiri <grijdaspiri07@gmail.com>
2018-09-23 23:33:03 -0700

    Finished Fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -1,1 +1,16 @@
 n = int(input('How many fibonacci numbers do you want? '))
+firstnum = 1
+secondnum = 1
+if n == 0:
+    print ('no number')
+elif n==1:
+    print (firstnum)
+else:
+    print (firstnum)
+    print (secondnum)
+
+for i in range (3,n+1):
+    num= firstnum+secondnum
+    print (num)
+    firstnum = secondnum
+    secondnum = num
